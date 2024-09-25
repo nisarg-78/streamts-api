955e78b677c7d97bf8977dbf5efe22de929388f8
nisarg-78 <gajjarnisarg78@gmail.com>
2024-09-25 21:34:53 +0530

    should work now i guess

--- src/videos/videos.controller.ts
+++ src/videos/videos.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, Param, Query } from '@nestjs/common';
 import { VideosService } from './videos.service';
 
 @Controller('videos')
@@ -6,12 +6,22 @@ export class VideosController {
   constructor(private readonly videosService: VideosService) {}
 
   @Get('all')
-  async findAll() {
-    return await this.videosService.getAllVideos();
+  async findAll(@Query("tags") tags: string) {
+    return await this.videosService.getAllVideos(tags);
   }
 
-  @Get(':id')
+  @Get('search')
+  async search(@Query("s") query: string) {
+    return await this.videosService.searchVideos(query);
+  }
+
+  @Get('info/:id')
   async findOne(@Param('id') id: string) {
+    return await this.videosService.getVideo(id);
+  }
+
+  @Get('master/:id')
+  async getMaster(@Param('id') id: string) {
     return await this.videosService.getMasterM3U8(id);
   }
 }
--- src/videos/videos.service.ts
+++ src/videos/videos.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, InternalServerErrorException } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
@@ -13,13 +13,36 @@ export class VideosService {
     @InjectModel('Video') private videoModel: Model<Video>
   ) {}
 
-  async getAllVideos() {
+  async getAllVideos(tags: string) {
     try {
-      const videos = await this.videoModel.find()
-      return videos
+      const tagsArr = tags?.split(',').filter((tag) => tag !== ''); 
+      if (tagsArr.length > 0) {
+        const videos = await this.videoModel.find({ tags: { $in: tagsArr } });
+        if (!videos) throw new BadRequestException('Videos not found');
+        return videos;
+      } else {
+        const videos = await this.videoModel.find();
+        return videos;
+      }
     } catch (error) {
       console.error("Error retrieving videos:", error)
     }
+  }
+
+  async searchVideos(query: string) {
+    try {
+      const videos = await this.videoModel.find({ title: { $regex: query, $options: 'i' } });
+      return videos;
+    } catch (error) {
+      console.log(error)
+      throw new InternalServerErrorException('Error retrieving videos');
+    }
+  }
+
+  async getVideo(id: string) {
+    const video = await this.videoModel.findOne({ id });
+    if (!video) throw new BadRequestException('Video not found');
+    return video;
   }
 
   async getMasterM3U8(id: string) {
